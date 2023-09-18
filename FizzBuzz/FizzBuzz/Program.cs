b7ecbcdbaa64373c90ce5e00027882dd4fdbad48
Tomasz Wolanski <tomasz.wolanski@avanade.com>
2023-09-18 23:16:24 +0200

    added some tests to uncomment

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -7,6 +7,15 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            //using var fb = new ILGPUFizzBuzzer();
+            //var fb = new Vector256FizzBuzzer();
+            //var fb = new BaselineFizzBuzzer();
+
+            //foreach (var f in  fb.Execute(100))
+            //{
+            //    Console.WriteLine(f);
+            //}
+
             BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
         }
     }
